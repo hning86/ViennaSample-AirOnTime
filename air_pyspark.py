28ab462b241056748bb87ef2f31ddf29b0b32221
hning86 <mit_hai@hotmail.com>
2017-05-20 09:14:05 -0400

    code

--- air_pyspark.py
+++ air_pyspark.py
@@ -1,53 +1,76 @@
-import numpy as np
-import pandas as pd
 import pyspark
-import os
-import urllib
+import pyspark.ml
 
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import VectorAssembler
+from pyspark.sql.types import DoubleType, IntegerType
+from pyspark.sql.functions import col
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
 from pyspark.sql.functions import *
-from pyspark.ml.classification import *
-from pyspark.ml.evaluation import *
-from pyspark.ml.feature import *
+from pyspark.ml.feature import OneHotEncoder, StringIndexer
+from azureml_sdk import data_collector
 
 # start Spark session
-spark = pyspark.sql.SparkSession.builder.appName('Iris').getOrCreate()
+spark = pyspark.sql.SparkSession.builder.appName('AirOnTime').getOrCreate()
 
-# load iris.csv into Spark dataframe
-data = spark.createDataFrame(pd.read_csv('iris.csv', header=None, names=['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']))
-print("First 20 rows of Iris dataset:")
-data.show(10)
+# instantiate a logger
+run_logger = data_collector.current_run() 
 
-# vectorize all numerical columns into a single feature column
-feature_cols = data.columns[:-1]
-assembler = pyspark.ml.feature.VectorAssembler(inputCols=feature_cols, outputCol='features')
-data = assembler.transform(data)
+# read csv folder from attached wasb
+air = spark.read.csv('wasb:///airontime/*.csv', header=True)
 
-# convert text labels into indices
-data = data.select(['features', 'class'])
-label_indexer = pyspark.ml.feature.StringIndexer(inputCol='class', outputCol='label').fit(data)
-data = label_indexer.transform(data)
+#data = air.sample(False, 0.00001, seed=123).where('ARR_DEL15 IS NOT NULL').select('MONTH', 'DAY_OF_WEEK', 'UNIQUE_CARRIER', 'ARR_DEL15')
+data = air.where('ARR_DEL15 IS NOT NULL').select('MONTH', 'DAY_OF_WEEK', 'UNIQUE_CARRIER', 'ARR_DEL15')
+data = data.withColumn('MONTH', data['MONTH'].cast(DoubleType()))
+data = data.withColumn('DAY_OF_WEEK', data['DAY_OF_WEEK'].cast(DoubleType()))
+# rename "ARR_DEL15" column to "label"
+data = data.withColumn('label', data['ARR_DEL15'].cast(DoubleType()))
 
-# only select the features and label column
-data = data.select(['features', 'label'])
-print("Reading for machine learning")
-data.show(10)
+# cache the raw dataset
+numOfRows = data.cache().count()
+print('Total number of rows: {}'.format(numOfRows))
 
-# use Logistic Regression to train on the training set
-train, test = data.randomSplit([0.70, 0.30])
-lr = pyspark.ml.classification.LogisticRegression()
-model = lr.fit(train)
+train, test = data.select('DAY_OF_WEEK', 'MONTH', 'UNIQUE_CARRIER', 'label').randomSplit([0.8, 0.2])
+# cache the training set
+print('train data size: {}'.format(train.cache().count()))
+# cache the test set
+print('test data size: {}'.format(test.cache().count()))
 
-# predict on the test set
-prediction = model.transform(test)
-print("Prediction")
-prediction.show(10)
+# string index and one-hot encode DAY_OF_WEEK column
+siDoW = StringIndexer(inputCol="DAY_OF_WEEK", outputCol="doWIndex")
+ecDow = OneHotEncoder(inputCol=siDoW.getOutputCol(), outputCol="e1")
 
-# evaluate the accuracy of the model using the test set
-evaluator = pyspark.ml.evaluation.MulticlassClassificationEvaluator(metricName='accuracy')
-accuracy = evaluator.evaluate(prediction)
+# string index and one-hot encode MONTH column
+siM = StringIndexer(inputCol='MONTH', outputCol='monthIndex')
+ecM = OneHotEncoder(inputCol=siM.getOutputCol(), outputCol="e2")
+
+# string index and one-hot encode UNIQUE_CARRIER column
+siCarrier = StringIndexer(inputCol='UNIQUE_CARRIER', outputCol='carrierIndex')
+ecC = OneHotEncoder(inputCol=siCarrier.getOutputCol(), outputCol='ca')
+
+# assemble vectors
+assembler = VectorAssembler(inputCols=[ecDow.getOutputCol(), ecM.getOutputCol(), ecC.getOutputCol()], outputCol="features")
+
+# Logistic Regression algorithm with default parameter settings
+lr = LogisticRegression()
+
+# create the pipeline
+pipe = Pipeline(stages=[siDoW, siM, ecDow, ecM, siCarrier, ecC, assembler, lr])
+
+# train
+model = pipe.fit(train)
+
+# score
+pred = model.transform(test)
+
+# evaluate
+bce = BinaryClassificationEvaluator(rawPredictionCol='rawPrediction')
+auc = bce.setMetricName('areaUnderROC').evaluate(pred)
 
 print()
-print('#####################################')
-print ("Accuracy is {}".format(accuracy))
-print('#####################################')
-print()
+print('######################################')
+print('AUC: {}'.format(auc))
+print('######################################')
+
+run_logger.log("AUC", auc)
