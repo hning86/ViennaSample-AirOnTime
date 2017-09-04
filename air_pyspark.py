b53171d8f976f5bfc26d8cbf5a7484b29fc7cc14
Hai Ning <hning86@users.noreply.github.com>
2017-09-03 23:19:44 -0400

    Update air_pyspark.py

--- air_pyspark.py
+++ air_pyspark.py
@@ -15,82 +15,71 @@ from pyspark.sql.functions import col
 from pyspark.ml.evaluation import BinaryClassificationEvaluator
 from pyspark.sql.functions import *
 from pyspark.ml.feature import OneHotEncoder, StringIndexer
-from azureml_sdk import data_collector
 
-# start Spark session
-spark = pyspark.sql.SparkSession.builder.appName('AirOnTime').getOrCreate()
-# instantiate a logger
-run_logger = data_collector.current_run() 
+class timeit():
+    from datetime import datetime
+    def __enter__(self):
+        self.tic = self.datetime.now()
+    def __exit__(self, *args, **kwargs):
+        print('Total running time: {}'.format(self.datetime.now() - self.tic))
 
-# read csv folder from attached wasb
-air = spark.read.csv('wasb:///airontime/*.csv', header=True)
+with timeit():   
+    # Start Spark application
+    spark = pyspark.sql.SparkSession.builder.appName("Air On Time").getOrCreate()
 
-# take a very small sample
-#data = air.sample(False, 0.00001, seed=123).where('ARR_DEL15 IS NOT NULL').select('MONTH', 'DAY_OF_WEEK', 'UNIQUE_CARRIER', 'CRS_ELAPSED_TIME', 'ARR_DEL15')
+    # Load all .csv files in airontime folder
+    air = spark.read.csv('wasb:///airontime/*.csv', header=True)    
 
-# load data from a local parquet file
-#air = spark.read.parquet('AirOnTime_sample_15k.parquet')
+    # cache and count
+    print("Total number of rows: {}".format(air.cache().count()))
+    
+    # remote missing values and select only 6 columsn out of 47
+    data = air\
+        .where('ARR_DEL15 IS NOT NULL AND CRS_ELAPSED_TIME IS NOT NULL')\
+        .select('MONTH', 'DAY_OF_WEEK', 'UNIQUE_CARRIER', 'DISTANCE', 'CRS_ELAPSED_TIME', 'ARR_DEL15')
 
-# select a list of relevant columns
-data = air.where('ARR_DEL15 IS NOT NULL AND CRS_ELAPSED_TIME IS NOT NULL').select('MONTH', 'DAY_OF_WEEK', 'UNIQUE_CARRIER', 'CRS_ELAPSED_TIME', 'ARR_DEL15')
+    # set correct datat types
+    data = data.withColumn('CRS_ELAPSED_TIME', data['CRS_ELAPSED_TIME'].cast(DoubleType()))
+    data = data.withColumn('DISTANCE', data['DISTANCE'].cast(DoubleType()))
+    data = data.withColumn('label', data['ARR_DEL15'].cast(DoubleType()))
+    print("Total number of rows after wrangling: {}".format(data.cache().count()))
+    data.printSchema()
+    
+    # split data to training and test sets
+    train, test = data.select('DAY_OF_WEEK', 'MONTH', 'DISTANCE', 'UNIQUE_CARRIER', 'label').randomSplit([0.7, 0.3], seed=123)
+    print('Number of rows in training dataset: {}'.format(train.cache().count()))
+    print('Number of rows in test data size: {}'.format(test.cache().count()))
+        
+    # string index and one-hot encode DAY_OF_WEEK column
+    siDoW = StringIndexer(inputCol="DAY_OF_WEEK", outputCol="doWIndex")
+    ecDow = OneHotEncoder(inputCol=siDoW.getOutputCol(), outputCol="e1")
 
-# convert CRS_ELAPSED_TIME to double.
-data = data.withColumn('CRS_ELAPSED_TIME', data['CRS_ELAPSED_TIME'].cast(DoubleType()))
+    # string index and one-hot encode MONTH column
+    siM = StringIndexer(inputCol='MONTH', outputCol='monthIndex')
+    ecM = OneHotEncoder(inputCol=siM.getOutputCol(), outputCol="e2")
 
-# rename "ARR_DEL15" column to "label", and cast it to double.
-data = data.withColumn('label', data['ARR_DEL15'].cast(DoubleType()))
+    # string index and one-hot encode UNIQUE_CARRIER column
+    siCarrier = StringIndexer(inputCol='UNIQUE_CARRIER', outputCol='carrierIndex')
+    ecC = OneHotEncoder(inputCol=siCarrier.getOutputCol(), outputCol='ca')
 
-# cache the raw dataset
-numOfRows = data.cache().count()
-print('Total number of rows: {}'.format(numOfRows))
+    # assemble vectors
+    assembler = VectorAssembler(inputCols=[ecDow.getOutputCol(), ecM.getOutputCol(), ecC.getOutputCol()], outputCol="features")
 
-# split training and test datasets
-train, test = data.select('DAY_OF_WEEK', 'MONTH', 'UNIQUE_CARRIER', 'CRS_ELAPSED_TIME', 'label').randomSplit([0.75, 0.25])
+    # create a Logistic Regression model
+    lr = LogisticRegression()
 
-# cache the training set
-print('train data size: {}'.format(train.cache().count()))
+    # create the pipeline
+    pipe = Pipeline(stages=[siDoW, siM, ecDow, ecM, siCarrier, ecC, assembler, lr])
 
-# cache the test set
-print('test data size: {}'.format(test.cache().count()))
+    # train
+    model = pipe.fit(train)
 
-# string index and one-hot encode DAY_OF_WEEK column
-siDoW = StringIndexer(inputCol="DAY_OF_WEEK", outputCol="doWIndex")
-ecDow = OneHotEncoder(inputCol=siDoW.getOutputCol(), outputCol="e1")
+    # score
+    pred = model.transform(test)
 
-# string index and one-hot encode MONTH column
-siM = StringIndexer(inputCol='MONTH', outputCol='monthIndex')
-ecM = OneHotEncoder(inputCol=siM.getOutputCol(), outputCol="e2")
+    # evaluate
+    bce = BinaryClassificationEvaluator(rawPredictionCol='rawPrediction')
+    auc = bce.setMetricName('areaUnderROC').evaluate(pred)
 
-# string index and one-hot encode UNIQUE_CARRIER column
-siCarrier = StringIndexer(inputCol='UNIQUE_CARRIER', outputCol='carrierIndex')
-ecC = OneHotEncoder(inputCol=siCarrier.getOutputCol(), outputCol='ca')
-
-# assemble numeric features into a single vector column named 'features'
-assembler = VectorAssembler(inputCols=['e1', 'e2', 'ca', 'CRS_ELAPSED_TIME'], outputCol="features")
-
-# Logistic Regression algorithm with default parameter settings
-lr = LogisticRegression(maxIter=50, regParam=0.01, elasticNetParam=0.8)
-
-# create the pipeline
-pipe = Pipeline(stages=[siDoW, siM, ecDow, ecM, siCarrier, ecC, assembler, lr])
-
-# train
-model = pipe.fit(train)
-
-# score
-pred = model.transform(test)
-
-# evaluate
-bce = BinaryClassificationEvaluator(rawPredictionCol='rawPrediction')
-auc = bce.setMetricName('areaUnderROC').evaluate(pred)
-
-print()
-print('######################################')
-print('AUC: {}'.format(auc))
-print('######################################')
-
-# log AUC
-run_logger.log("AUC", auc)
-
-# save the model
-model.write().overwrite().save("wasb:///models/AirOnTimeModel.mml")
+    # print AUC
+    print('AUC: {}'.format(auc))
